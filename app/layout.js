5afba075aa3a4ac214eaaa5e849e6b9f6f6e11c7
Unknownaod <unknown@dona.pw>
2026-09-17 00:26:14 -0400

    Update metadata title and description

--- app/layout.js
+++ app/layout.js
@@ -12,8 +12,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Fades Mail",
-  description: "Your perfect mail source",
+  title: "Fades Systems",
+  description: "The home of the Fades Ecosystem",
 };
 
 export default function RootLayout({ children }) {
